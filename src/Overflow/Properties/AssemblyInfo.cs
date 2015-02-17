dd530ef7c41e0699cb9d030763be898dac95ae73
Morten Christiansen <withoutprecedence@gmail.com>
2015-02-17 18:11:03 +0100

    Removed GitVersion until a way can be found to make it work with Travis.

--- src/Overflow/Properties/AssemblyInfo.cs
+++ src/Overflow/Properties/AssemblyInfo.cs
@@ -10,3 +10,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
