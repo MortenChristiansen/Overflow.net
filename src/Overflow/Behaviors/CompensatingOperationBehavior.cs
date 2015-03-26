85d123ab52e04aef7b1069230af1ca21b31bc7b6
Morten Christiansen <withoutprecedence@gmail.com>
2015-03-26 21:09:32 +0100

    Initial behavior code for compensating operation behavior.

---
+++ src/Overflow/Behaviors/CompensatingOperationBehavior.cs
@@ -0,0 +1,29 @@
+using System;
+using Overflow.Extensibility;
+
+namespace Overflow.Behaviors
+{
+    class CompensatingOperationBehavior : OperationBehavior
+    {
+        private readonly IOperation _operation;
+
+        public override BehaviorPrecedence Precedence
+        {
+            get { return BehaviorPrecedence.WorkCompensation; }
+        }
+
+        public CompensatingOperationBehavior(IOperation operation)
+        {
+            _operation = operation;
+        }
+
+        public override void Execute()
+        {
+            try { base.Execute(); }
+            catch
+            {
+                _operation.Execute();
+            }
+        }
+    }
+}
---
+++ test/Overflow.Test/Behaviors/CompensatingOperationBehaviorTests.cs
@@ -0,0 +1,90 @@
+using System;
+using Overflow.Behaviors;
+using Overflow.Extensibility;
+using Overflow.Test.Fakes;
+using Xunit;
+
+namespace Overflow.Test.Behaviors
+{
+    public class CompensatingOperationBehaviorTests
+    {
+        [Fact]
+        public void The_behavior_has_work_compensation_level_precedence()
+        {
+            var sut = new CompensatingOperationBehavior(new FakeOperation());
+
+            Assert.Equal(BehaviorPrecedence.WorkCompensation, sut.Precedence);
+        }
+
+        [Fact]
+        public void Errors_compensated_for_are_not_rethrown()
+        {
+            var operation = new FakeOperation { ThrowOnExecute = new Exception() };
+            var sut = new CompensatingOperationBehavior(new FakeOperation());
+            sut.Attach(operation);
+
+            sut.Execute();
+        }
+
+        [Fact]
+        public void When_an_error_occurs_in_the_operation_the_compensating_operation_is_immediately_executed()
+        {
+            var operation = new FakeOperation { ThrowOnExecute = new Exception() };
+            var compensatingOperation = new FakeOperation();
+            var sut = new CompensatingOperationBehavior(compensatingOperation);
+            sut.Attach(operation);
+
+            sut.Execute();
+
+            Assert.True(compensatingOperation.HasExecuted);
+        }
+
+        // What about the execution context of the compensating operation? And input data? And what about the
+        // impact of retry behaviors?
+
+        /* Questions
+         * - Should the CO have input data?
+         * - Should the CO provide output data?
+         * - Should the CO be allowed the full behavior support?
+         * - How to handle failures in the CO? It would not be logged as a failure if the logging behavior is not applied
+         * - How to handle multiple applications of the behavior? Disallow? I think we should disallow multiple values to keep it simple
+         * 
+         * I think there are too many issues running the operation outside of the normal flow. I would have to think about how every future
+         * feature works with it. Just adding another operation to the normal execution floor does not break anything. We would of course
+         * log the event that the compensating operation is scheduled.
+         * 
+         * Another approach is to let the compensating operation be a simple Func<bool>. We execute it in the catch block and if it returns
+         * false, we rethrow the exception. We would have to add a way to inject any needed parameters. It could be done by adding the
+         * configuration as a parameter to the OperationBehaviorAttribute.CreateBehavior method. It annoys me that I won't have the full
+         * power of operations. The compensating operation could have any number of child operations, supporting arbitrarily complex 
+         * logic, with full logging and other behaviors. Would the func need to be executed in its own try/catch and what would happen
+         * on error?
+         * 
+         * The attribute should take optional parameters for filtering errors by type. So the result is a new abstract attribute type
+         * inheriting from OperationBehaviorAttribute which has an abstract method for providing the Func<bool>.
+         * 
+         * Scheduling operations:
+         * 
+         * public override void OnExecute()
+         * {
+         *     Schedule<OperationA>(); // What about input/output data?
+         * }
+         * 
+         * in behavior:
+         * 
+         * public override void Execute()
+         * {
+         *     try
+         *     {
+         *         base.Execute();   
+         *     }
+         *     catch
+         *     {
+         *          Schedule<OperationA>(); // It cannot be private to Operation then
+         *          
+         *          base.Schedule<OperationA>(); // Alternative if the method is public 
+         *     }
+         * }
+         */
+    }
+}
