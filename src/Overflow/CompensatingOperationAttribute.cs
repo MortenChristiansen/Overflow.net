9d4f95cba56d2509ed740489af44340528ffe6a4
Morten Christiansen <withoutprecedence@gmail.com>
2015-04-01 17:45:42 +0200

    Added missing comments to public type.

--- src/Overflow/CompensatingOperationAttribute.cs
+++ src/Overflow/CompensatingOperationAttribute.cs
@@ -6,11 +6,34 @@ using Overflow.Utilities;
 
 namespace Overflow
 {
+    /// <summary>
+    /// Applies the Compensating Operation beahvior. When errors occur during the execution
+    /// of the operation, an instance of the specified operation type is created and executed.
+    /// This operation gets behaviors applied as normal and can implement the IInputOperation
+    /// interface. The IOutputOperation interface will be ignored.
+    /// 
+    /// Note that the exception thrown in the original exception is rethrown once the 
+    /// compensating operation is done executing. Consider using the ContinueOnFailure attribute
+    /// if this is not the desired behavior.
+    /// 
+    /// If the compensating operation throws an exception, the original exception is not rethrown,
+    /// and the new exception bubbles up instead. To avoid this, make the compensating operation
+    /// use the ContinueOnFailure attribute.
+    /// </summary>
     public class CompensatingOperationAttribute : OperationBehaviorAttribute
     {
         private readonly Type _operationType;
         private readonly Type[] _compensatedExceptionTypes;
 
+        /// <summary>
+        /// Creates a new instance of the attribute.
+        /// </summary>
+        /// <param name="operationType">The type of compensating operation to create in
+        /// case of exceptions. The type must implement the IOperation interface.</param>
+        /// <param name="compensatedExceptionTypes">Optionally supply specific exception types
+        /// to limit the compensating operations to. By default, all exceptions are compensated
+        /// for. Exceptions inheriting from one of the specified types will be compensated for 
+        /// as well. The types must inherit from Exception.</param>
         public CompensatingOperationAttribute(Type operationType, params Type[] compensatedExceptionTypes)
         {
             Verify.NotNull(operationType, "operationType");
@@ -20,6 +43,10 @@ namespace Overflow
             _compensatedExceptionTypes = compensatedExceptionTypes;
         }
 
+        /// <summary>
+        /// Creates a new OperationBehavior instance.
+        /// </summary>
+        /// <param name="configuration">The configuration of the executing workflow</param>
         public override OperationBehavior CreateBehavior(WorkflowConfiguration configuration)
         {
             return new CompensatingOperationBehavior(CreateCompensatingOperation(configuration), _compensatedExceptionTypes);
