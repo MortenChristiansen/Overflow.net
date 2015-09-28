53e905e81813b97aba90799544e24426d23488d5
Morten Christiansen <withoutprecedence@gmail.com>
2015-09-28 22:29:06 +0200

    Added the original attribute back in, with an Obsolete attribute.

---
+++ src/Overflow/IndempotentAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Overflow
+{
+    /// <summary>
+    /// Marks the operation as being idempotent. This provides information to
+    /// behaviors regarding whether it can be executed more than once.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    [Obsolete("The attribute was renamed to fix a typo. Use IdempotentAttribute instead. This class will be removed in verson 2.0.")]
+    public class IndempotentAttribute : IdempotentAttribute
+    {
+    }
+}
