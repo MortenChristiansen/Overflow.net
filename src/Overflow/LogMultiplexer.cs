dab2c492d18d04ce45858c49a28afd70c8607c30
Morten Christiansen <withoutprecedence@gmail.com>
2015-10-14 21:36:42 +0200

    Added multiplexing logger.

---
+++ src/Overflow/LogMultiplexer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Overflow.Extensibility;
+using Overflow.Utilities;
+
+namespace Overflow
+{
+    /// <summary>
+    /// A logger that forwards logs to any number of actual loggers. Use this if
+    /// you need to log to more than one destination.
+    /// </summary>
+    public class LogMultiplexer : IWorkflowLogger
+    {
+        private readonly List<IWorkflowLogger> _loggers;
+
+        /// <summary>
+        /// Creates a new instance of the log multiplexer.
+        /// </summary>
+        /// <param name="loggers">Logger implementations to invoke
+        /// whenever a log entry is added.</param>
+        public LogMultiplexer(params IWorkflowLogger[] loggers)
+        {
+            Verify.NotNull(loggers, nameof(loggers));
+
+            _loggers = loggers.ToList();
+        }
+
+        /// <summary>
+        /// Call OperationStarted on each supplied logger.
+        /// </summary>
+        /// <param name="operation">The operation is passed on the each logger</param>
+        public void OperationStarted(IOperation operation)
+        {
+            Verify.NotNull(operation, nameof(operation));
+
+            _loggers.ForEach(l => l.OperationStarted(operation));
+        }
+
+        /// <summary>
+        /// Call OperationFinished on each supplied logger.
+        /// </summary>
+        /// <param name="operation">The operation is passed on the each logger</param>
+        /// <param name="duration">The duration is passed on the each logger</param>
+        public void OperationFinished(IOperation operation, TimeSpan duration)
+        {
+            Verify.NotNull(operation, nameof(operation));
+
+            _loggers.ForEach(l => l.OperationFinished(operation, duration));
+        }
+
+        /// <summary>
+        /// Call OperationFailed on each supplied logger.
+        /// </summary>
+        /// <param name="operation">The operation is passed on the each logger</param>
+        /// <param name="error">The error is passed on the each logger</param>
+        public void OperationFailed(IOperation operation, Exception error)
+        {
+            Verify.NotNull(operation, nameof(operation));
+            Verify.NotNull(error, nameof(error));
+
+            _loggers.ForEach(l => l.OperationFailed(operation, error));
+        }
+
+        /// <summary>
+        /// Call BehaviorWasApplied on each supplied logger.
+        /// </summary>
+        /// <param name="operation">The operation is passed on the each logger</param>
+        /// <param name="behavior">The behavior is passed on the each logger</param>
+        /// <param name="description">The description is passed on the each logger</param>
+        public void BehaviorWasApplied(IOperation operation, OperationBehavior behavior, string description)
+        {
+            Verify.NotNull(operation, nameof(operation));
+            Verify.NotNull(behavior, nameof(behavior));
+            Verify.NotNull(description, nameof(description));
+
+            _loggers.ForEach(l => l.BehaviorWasApplied(operation, behavior, description));
+        }
+    }
+}
---
+++ test/Overflow.Test/LogMultiplexerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Overflow.Extensibility;
+using Overflow.Test.Fakes;
+using Overflow.Test.TestingInfrastructure;
+using Xunit;
+
+namespace Overflow.Test
+{
+    public class LogMultiplexerTests : TestBase
+    {
+        [Fact]
+        public void Guards_are_verified()
+        {
+            VerifyGuards<LogMultiplexer>();
+        }
+
+        [Fact]
+        public void You_can_create_a_log_multiplexer()
+        {
+            new LogMultiplexer();
+        }
+
+        [Theory, AutoMoqData]
+        public void Started_operations_are_forwarded_to_every_supplied_loggers(FakeWorkflowLogger log1, FakeWorkflowLogger log2, IOperation operation)
+        {
+            var sut = new LogMultiplexer(log1, log2);
+
+            sut.OperationStarted(operation);
+
+            Assert.Equal(1, log1.StartedOperations.Count);
+            Assert.Same(operation, log1.StartedOperations[0]);
+            Assert.Equal(1, log2.StartedOperations.Count);
+            Assert.Same(operation, log2.StartedOperations[0]);
+        }
+
+        [Theory, AutoMoqData]
+        public void Finished_operations_are_forwarded_to_every_supplied_loggers(FakeWorkflowLogger log1, FakeWorkflowLogger log2, IOperation operation)
+        {
+            var sut = new LogMultiplexer(log1, log2);
+
+            sut.OperationFinished(operation, TimeSpan.FromSeconds(1));
+
+            Assert.Equal(1, log1.FinishedOperations.Count);
+            Assert.Same(operation, log1.FinishedOperations[0]);
+            Assert.Equal(TimeSpan.FromSeconds(1), log1.FinishedOperationDurations[0]);
+            Assert.Equal(1, log2.FinishedOperations.Count);
+            Assert.Same(operation, log2.FinishedOperations[0]);
+            Assert.Equal(TimeSpan.FromSeconds(1), log2.FinishedOperationDurations[0]);
+        }
+
+        [Theory, AutoMoqData]
+        public void Failed_operations_are_forwarded_to_every_supplied_loggers(FakeWorkflowLogger log1, FakeWorkflowLogger log2, IOperation operation, Exception error)
+        {
+            var sut = new LogMultiplexer(log1, log2);
+
+            sut.OperationFailed(operation, error);
+
+            Assert.Equal(1, log1.OperationFailures.Count);
+            Assert.Same(error, log1.OperationFailures[operation][0]);
+            Assert.Equal(1, log2.OperationFailures.Count);
+            Assert.Same(error, log2.OperationFailures[operation][0]);
+        }
+
+        [Theory, AutoMoqData]
+        public void Applied_behaviors_are_forwarded_to_every_supplied_loggers(FakeWorkflowLogger log1, FakeWorkflowLogger log2, IOperation operation, OperationBehavior behavior, string description)
+        {
+            var sut = new LogMultiplexer(log1, log2);
+
+            sut.BehaviorWasApplied(operation, behavior, description);
+
+            Assert.Equal(1, log1.AppliedBehaviors.Count);
+            Assert.Same(behavior, log1.AppliedBehaviors[0].Behavior);
+            Assert.Equal(description, log1.AppliedBehaviors[0].Description);
+            Assert.Same(operation, log1.AppliedBehaviors[0].Operation);
+            Assert.Equal(1, log2.AppliedBehaviors.Count);
+            Assert.Same(behavior, log2.AppliedBehaviors[0].Behavior);
+            Assert.Equal(description, log2.AppliedBehaviors[0].Description);
+            Assert.Same(operation, log2.AppliedBehaviors[0].Operation);
+        }
+    }
+}
